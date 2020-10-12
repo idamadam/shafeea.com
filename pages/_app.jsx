c85d589fcba52107961d5e7daccdd5f3657108d4
Idam Adam <contact@idamadam.com>
2020-10-12 17:45:07 +1100

    Use normalise & typewriter font

---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+import "normalize.css/normalize.css";
+import "../fonts/fonts.css";
+
+function AppBase({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default AppBase;
