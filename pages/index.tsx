20f5682101df192b3bcc084d51346b33c786bcbb
Idam Adam <contact@idamadam.com>
2021-02-13 13:32:31 +1100

    Add all poems

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,9 +52,16 @@ function HomePage() {
       <Global styles={globalStyles} />
       <div>
         <h1>ޝަފީއާގެ ލިޔުން</h1>
-        <PostListing title="ހާދަރަނގަޅޭ ޝުކުރިޔާ!" link="/lhen/shukuriyya" date="10 އޮކްޓޮބަރު 2020" />
+        <PostListing title="ލޯބިވާ މަންމަ އަށް ޝިފާއަށް އެދި........!" link="/lhen/shifa" date="21 އޯގަސްޓް 2020" />
+        <PostListing title="ހިއްވަރުކުރޭ!" link="/lhen/hiyvarukurey" date="9 ސެޕްޓެމްބަރު 2020" />
+        <PostListing title="މަންމަ ދަރުމަވަންތަ ހޮސްޕިޓަލުގެ 11 ވަނަ ފަންގިފިލާ ގައި........" link="/lhen/dharumavantha" date="29 ސެޕްޓެމްބަރު 2020" />
+        <PostListing title="ހާދަރަނގަޅޭ ޝުކުރިޔާ!" link="/lhen/shukuriyya" date="10 އޮކްޓޯބަރު 2020" />
+        <PostListing title="މަންމަ ގެ ދަރިން" link="/lhen/dharin" date="3 ނޮވެމްބަރު 2020" />
         <PostListing title="ޅަފަތުގެ ހިޔާ" link="/lhen/lhafathuge" date="14 ނޮވެމްބަރު 2020" />
         <PostListing title="އަޅެފަހެ ކޮރޯނާ" link="/lhen/corona" date="19 ނޮވެމްބަރު 2020" />
+        <PostListing title="އިންތިހާ!" link="/lhen/inthihaa" date="27 ނޮވެމްބަރު 2020" />
+        <PostListing title="ކީއްވެގެންހޭ؟" link='/lhen/keevegenhey' date="29 ނޮވެމްބަރު 2020" />
+        <PostListing title="މިސް ވަނީ!" link="lhen/misvanee" date="1 ޑިސެމްބަރު 2020"/>
       </div>
     </div>
   );
